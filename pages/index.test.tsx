52fb9ff8659c069c8aa7114de50edac392849564
Daveed <daveed@santocabron.com>
2024-01-16 15:59:10 -0500

    added test files

---
+++ pages/index.test.tsx
@@ -0,0 +1,8 @@
+// import { createMocks, RequestMethod } from 'node-mocks-http';
+// import type { NextApiRequest, NextApiResponse } from 'next';
+// import page from './index';
+
+describe('/pages/', () => {
+  test.todo('Page renders');
+  test.todo('Link to patient present');
+});
---
+++ pages/patient.test.tsx
@@ -0,0 +1,6 @@
+describe('/pages/patient', () => {
+  test.todo('Page renders');
+  test.todo('Link to home page present');
+  test.todo('Patient summary present')
+  test.todo('Patient summary correctly displays homer information')
+});
