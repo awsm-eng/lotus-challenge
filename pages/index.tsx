d254054e7b62a893f3cfec04492ed3522a2e00ad
Rohit Gupta <rohit.gupta17@aexp.com>
2024-03-17 21:58:19 +0530

    feat(home-page): Home Page for Patient Information

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,24 @@
 import Link from 'next/link';
-import { Title } from '@mantine/core';
+import { Center, NavLink, Title } from '@mantine/core';
 import { Container } from '@medplum/react';
+import AppContainer from '../components/AppContainer';
 
 export default function IndexPage(): JSX.Element {
-
   return (
-    <Container mt="xl">
-      <Title>Welcome to Lotus AI</Title>
-      <Link href="/patient">Patient Summary</Link>
-    </Container>
+    <AppContainer>
+      <Container mt="xl">
+        <Center>
+          <Title>Welcome to Lotus AI</Title>
+        </Center>
+        <NavLink
+          href="/patient"
+          component={Link}
+          label="Patient Summary"
+          w={200}
+          variant="light"
+          active
+        />
+      </Container>
+    </AppContainer>
   );
-};
+}
